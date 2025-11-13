cc21ac9bfe7358c8180062872b9eb7c1343371f6
Tarun Shivakumar <tarunshivakumar1@gmail.com>
2025-11-13 01:18:52 -0500

    Add Excel logging to SysMonitor and remove old text output

--- SysMonitor.py
+++ SysMonitor.py
@@ -2,41 +2,57 @@ import psutil as util
 import sched
 import time
 import os
-import pandas as pd
+import xlwt
+from xlwt import Workbook
 
-def sys_output(scheduler, f, start_time):
+row = 1  # next Excel row to write
+
+def sys_output(scheduler, start_time):
+    global row, sheet1, wb, excel_path
 
     current_time = time.time()
 
     # stop after 60 seconds
     if current_time - start_time >= 60:
+        wb.save(excel_path)
         return
 
-    # do your system logging
-    cpu_percent = str(util.cpu_percent(1))
-    memory_percent = str(util.virtual_memory()[2])
-    disk_percent = str(util.disk_usage('/')[3])
-    data = (cpu_percent, memory_percent, disk_percent)
-    f.write(cpu_percent + ", " + memory_percent + ", " + disk_percent + "\n")
+    # capture metrics
+    cpu_percent = util.cpu_percent(interval=1)
+    memory_percent = util.virtual_memory().percent
+    disk_percent = util.disk_usage('C:\\').percent
 
-    f.flush()
-    os.fsync(f.fileno())
+    # write one sample per row: (CPU, Memory, Disk)
+    sheet1.write(row, 0, cpu_percent)
+    sheet1.write(row, 1, memory_percent)
+    sheet1.write(row, 2, disk_percent)
+
+    row += 1
+
+    # save workbook so data is visible while running
+    wb.save(excel_path)
 
     # schedule next run in 5 seconds
-    scheduler.enter(5, 1, sys_output, (scheduler, f, start_time))
+    scheduler.enter(5, 1, sys_output, (scheduler, start_time))
+
 
 if __name__ == '__main__':
-    path = 'C:/Users/tarun/OneDrive/Desktop/Projects/System Resource Monitor/Output Data.txt'
 
-    f = open(path, 'w')
+    wb = Workbook()
+    sheet1 = wb.add_sheet('Sheet 1')
 
-    f.write('CPU Percentage, Memory Percentage, Disk Percentage\n')
+    # header row
+    sheet1.write(0, 0, 'CPU Percentage')
+    sheet1.write(0, 1, 'Memory Percentage')
+    sheet1.write(0, 2, 'Disk Percentage')
+
+    excel_path = 'C:/Users/tarun/OneDrive/Desktop/Projects/System Resource Monitor/system_output.xls'
 
     s = sched.scheduler(time.time, time.sleep)
 
     start_time = time.time()   # when the logging began
     # first run happens immediately
-    s.enter(0, 1, sys_output, (s, f, start_time))
+    s.enter(0, 1, sys_output, (s, start_time))
     s.run()
 
-    f.close()
+    wb.save(excel_path)
